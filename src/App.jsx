2b771f58913342e2bb119a347af663d28e43d484
artyomakulov <artyomakulov@gmail.com>
2023-10-25 12:03:14 +0300

    add faq

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Header } from 'components/Header/Header';
 import { Hero } from 'components/Hero/Hero';
 import { Main } from 'components/Main/Main';
 import { Reviews } from 'components/Reviews/Reviews';
+import { FAQ } from 'components/FAQ/FAQ';
 export const App = () => {
   return (
     <>
       <Header />
       <Hero />
-      <Main/>
-      <Reviews/>
+      <Main />
+      <Reviews />
+      <FAQ />
     </>
   );
 };
---
+++ src/components/FAQ/FAQ.jsx
@@ -0,0 +1,84 @@
+import css from './FAQ.module.css';
+import plus from '../../images/plus.svg';
+import { useState } from 'react';
+
+export const FAQ = () => {
+  const [openQuestion, setOpenQuestion] = useState(null);
+
+
+  const toggleAnswer = (questionId) => {
+    setOpenQuestion(openQuestion === questionId ? null : questionId);
+  };
+
+  const faqData = [
+    {
+      id: 1,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 1.',
+    },
+    {
+      id: 2,
+      question: 'What platforms will I be able to use?',
+      answer: 'We provide only the best trading software. The trading terminal MetaTrader4 is the most popular and convenient platform for access to global exchanges. You can work anywhere. All you need is the Internet because the necessary trading tools are already collected in one place and are available in a couple of clicks. Use only the best and develop with us!',
+    },
+    {
+      id: 3,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 3.',
+    },
+    {
+      id: 4,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 4.',
+    },
+    {
+      id: 5,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 5.',
+    },
+    {
+      id: 6,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 6.',
+    },
+    {
+      id: 7,
+      question: 'How do I change my details?',
+      answer: 'Answer for question 7.',
+    },
+  ];
+
+  return (
+    <>
+      <div className={css.container_faq}>
+        <div className={css.container}>
+          <h2 className={css.title}>
+            Frequently asked <br />
+            <span className={css.title_highlight}>questions</span>
+          </h2>
+          <div>
+            <ul>
+              {faqData.map((item) => (
+                <li className={css.item} key={item.id}>
+                  <div className={css.question_form}>
+                    <p className={css.question}>{item.question}</p>
+                    <button onClick={() => toggleAnswer(item.id)}>
+                      <img src={plus} alt="open" width={32} />
+                    </button>
+                  </div>
+                  {openQuestion === item.id && (
+                    <p className={css.answer}>{item.answer}</p>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+
+
