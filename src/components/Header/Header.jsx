e13c8347cc13289a4a23b6acce7ceef279c056e6
artyomakulov <artyomakulov@gmail.com>
2023-10-25 23:28:40 +0300

    add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import css from './Header.module.css';
 import vector from '../../images/vector.svg';
 import burger from '../../images/burger.svg';
+import { useState } from 'react';
 
 export const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
     <header>
       <div className={css.container_header}>
@@ -55,9 +61,49 @@ export const Header = () => {
           </button>
           <button className={css.button_sign}>Sign in for free</button>
         </div>
-        <button className={css.burger}>
-          <img src={burger} alt="burger menu" width={24} />
-        </button>
+        <div className={css.burger_container}>
+          <button className={css.burger} onClick={toggleMenu}>
+            <img src={burger} alt="burger menu" width={24} />
+          </button>
+          {isMenuOpen && (
+            <div className={css.mobileMenu}>
+              <nav>
+                <ul className={css.list}>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      How it works
+                    </a>
+                  </li>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      About
+                    </a>
+                  </li>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      Instructions
+                    </a>
+                  </li>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      Accounts
+                    </a>
+                  </li>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      Platforms
+                    </a>
+                  </li>
+                  <li className={css.nav_item}>
+                    <a href="/" className={css.link}>
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
