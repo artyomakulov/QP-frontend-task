85bb243bc51c11dc2f7940de94dd93359efda33e
artyomakulov <artyomakulov@gmail.com>
2023-10-25 20:49:34 +0300

    add tablet main and reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -15,19 +15,19 @@ export const Reviews = () => {
               People love Big <br />
               <span className={css.title_highlight}>Invest</span>
             </h2>
+            <button className={`${css.buttons2} ${css.leftBtn}`}>
+              <img src={sliderLeft} alt="slider left" width={24} />
+            </button>
             <div className={css.slider}>
-              <button className={css.button}>
+              <button className={css.buttons}>
                 <img src={sliderLeft} alt="slider left" width={24} />
               </button>
-              <button className={css.button}>
+              <button className={css.buttons}>
                 <img src={sliderRight} alt="slider right" width={24} />
               </button>
             </div>
           </div>
           <div className={css.reviewer}>
-            <div className={css.quotes}>
-              <img src={quotes} alt="quotes" width={33} height={27} />
-            </div>
             <div className={css.person}>
               <div className={css.avatar}>
                 <img src={reviewer} alt="reviewer" width={73} />
@@ -37,17 +37,22 @@ export const Reviews = () => {
                 <p className={css.position}>Director of Growth</p>
               </div>
             </div>
+            <div className={css.quotes}>
+              <img src={quotes} alt="quotes" width={33} height={27} />
+            </div>
             <p className={css.text}>
               This magical product actually works! It has radically <br />
               changed the way we build our audiences. <br /> Increasing new
               customer sales by 6x in our most <br /> mature market.
             </p>
-
-            <div>
+            <div className={css.numbers}>
               <span className={css.counter_first}>01 </span>
               <span className={css.counter_second}>/ 04</span>
             </div>
           </div>
+          <button className={`${css.buttons2} ${css.rightBtn}`}>
+            <img src={sliderRight} alt="slider right" width={24} />
+          </button>
         </div>
       </div>
     </>
